c4ada0b1de62e7633c20166a3d59b30f1e8a3790
luciano.federico.olmedo <luciano.federico.olmedo@gmail.com>
2015-10-24 22:52:52 -0300

    Add KernelTest

---
+++ test/KernelTests/TestKernel.py
@@ -0,0 +1,29 @@
+__author__ = 'luciano'
+
+import unittest
+from src.Kernel.Kernel import *
+from src.Cpu.Interruption import *
+from src.Cpu.InterruptionManager import *
+from src.PCB.PCB import *
+
+
+class TestScheduler(unittest.TestCase):
+
+    def setUp(self):
+        self.kernel = Kernel(None)
+        self.kernel.set_interruption_manager(InterruptionManager(None))
+        self.kernel.to_kernel_mode
+
+    def load_handlers(self):
+        for interruption in [(IOInterruption,IOInterruptionManager),(KillInterruption,KillInterruptionManager),
+         (WaitingInterruption,WaitingInterruptionManager),(EndIOInterruption,EndIOInterruptionManager),
+         (NewInterruption,NewInterruptionManager),(TimeoutInterruption,TimeoutInterruptionManager)]:
+            self.kernel.add_handlers(interruption[0],interruption[1])
+
+    def test_when_added_handlers_this_work_fine(self):
+        self.load_handlers()
+        for interruption in [IOInterruption,KillInterruption, WaitingInterruption,EndIOInterruption,
+         NewInterruption ,TimeoutInterruption]:
+            self.assertEqual(interruption, self.kernel.interruption_manager.manager_for(interruption))
+
+
