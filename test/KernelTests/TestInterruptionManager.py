6d305a09425a81acd29091194ad8c5be3134eb13
luciano.federico.olmedo <luciano.federico.olmedo@gmail.com>
2015-10-24 23:15:44 -0300

    Refac Name to TestInterruptionManager

--- test/KernelTests/TestInterruptionManager.py
+++ test/KernelTests/TestInterruptionManager.py
@@ -1,18 +1,14 @@
 __author__ = 'luciano'
 
 import unittest
-from src.Kernel.Kernel import *
 from src.Cpu.Interruption import *
 from src.Cpu.InterruptionManager import *
-from src.PCB.PCB import *
 
 
-class TestScheduler(unittest.TestCase):
+class TestInterruptionManager(unittest.TestCase):
 
     def setUp(self):
-        self.kernel = Kernel(None)
-        self.kernel.set_interruption_manager(InterruptionManager(None))
-        self.kernel.to_kernel_mode
+        self.interruption_manager= InterruptionManager(None)
 
     def load_handlers(self):
         for interruption in [(IOInterruption,IOInterruptionManager),(KillInterruption,KillInterruptionManager),
@@ -25,6 +21,6 @@ class TestScheduler(unittest.TestCase):
         for interruption in [(IOInterruption,IOInterruptionManager),(KillInterruption,KillInterruptionManager),
          (WaitingInterruption,WaitingInterruptionManager),(EndIOInterruption,EndIOInterruptionManager),
          (NewInterruption,NewInterruptionManager),(TimeoutInterruption,TimeoutInterruptionManager)]:
-            self.assertEqual(interruption[1], self.kernel.interruption_manager.manager_for(interruption[0]))
+            self.assertEqual(interruption[1], self.interruption_manager.manager_for(interruption[0]))
 
 
