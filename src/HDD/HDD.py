89bf5a9bf405162c9a89b43709312a185d9503bc
lucianofedericoolmedo <luciano.federico.olmedo@gmail.com>
2015-11-10 16:44:52 -0300

    Refac but still not working!!!

--- src/HDD/HDD.py
+++ src/HDD/HDD.py
@@ -1,6 +1,6 @@
 __author__ = 'luciano'
 
-import jsonpickle
+
 from src.HDD.FileSystemComponents import *
 from src.HDD.FileSystem import FileSystem
 from src.HDD.DriveSaver import DriveSaver
@@ -11,27 +11,35 @@ class HDD:
     def __init__(self, amount_sector):
         self._drive_saver = DriveSaver(self)
         self._sectors = dict.fromkeys(range(1, amount_sector), [])
-        self._representation = jsonpickle.encode(FileSystem(self._drive_saver, Folder(None, "/")))
+        self._representation = None
+        #jsonpickle.encode(FileSystem(self._drive_saver, Folder(None, "/")))
         self._swap_area = []
 
     def get_drive_saver(self):
         return self._drive_saver
 
     def get_blocks(self, token):
-        return map(lambda x: self._sectors[unicode(token.get_sector())][x - 1], token.get_blocks())
+        pass
+        #return map(lambda x: self._sectors[unicode(token.get_sector())][x - 1], token.get_blocks()                    )
 
     def add_block(self, sector, block):
+        pass
+        '''
+        quiero utilizar algo copado para mostrar directorios
         self._sectors[unicode(sector)].append(block)
         return len(self._sectors[unicode(sector)])
+        '''
 
     def sectors_size(self):
         return len(self._sectors.keys())
 
     def generate_file_system(self):
-        return jsonpickle.decode(self._representation)
+        pass
+        #return jsonpickle.decode(self._representation)
 
     def serialize_file_system(self, file_system):
-        self._representation = jsonpickle.encode(file_system)
+        pass
+        #self._representation = jsonpickle.encode(file_system)
 
     def find_page(self, index):
         page = filter(lambda x: x.get_index() != index, self._swap_area )
